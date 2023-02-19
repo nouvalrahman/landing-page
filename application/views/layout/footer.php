7643b45b54a49ab0cf1afc8b902362de13dd6771
nouvalrahman <nouvalrahman28@gmail.com>
2023-02-19 14:47:32 +0700

    update css 2

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -2,4 +2,10 @@
     <a href="https://api.whatsapp.com/send?phone=085334630958&text=Assalamualaikum%20Wr.%20Wb%20Saya%20Bertanya%20tentang." class="float" target="_blank">
         <i class="fa fa-whatsapp my-float"></i>
     </a>
+    <div class="footer-links">
+		<ul class="list-inline">
+			<li>Copyright © 2023 All rights reserved | <a class="font-weight-bold" href="https://smam1ta.sch.id/" target="_blank">ICT SMAMITA</a>
+			</li>
+		</ul>
+	</div>
 </footer>
